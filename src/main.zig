5ade89d23cac2e01b0621ad378e70e27fd957c5a
RConant6 <rconant6@gmail.com>
2025-07-25 14:55:25 -0400

    [MAIN] functional quit 'action'

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,69 @@
 const std = @import("std");
-const engine = @import("engine");
+const eng = @import("engine");
+const Engine = eng.Engine;
+const EngineConfig = eng.EngineConfig;
+const InputManager = eng.InputManager;
 
-const Config = struct {
-    const TARGET_FPS: f32 = 60.0;
-    const TARGET_FRAME_TIME_US: i64 = @intFromFloat((1.0 / TARGET_FPS) * 1_000_000);
-    const WIDTH: i32 = 1600;
-    const HEIGHT: i32 = 900;
+// const GameActions = enum { Quit, Thrust, RotateLeft, RotateRight, Shoot };
+const GameActions = enum { Quit };
+
+const Game = struct {
+    actions: eng.input.ActionManager(GameActions),
+
+    pub fn init(allocator: std.mem.Allocator, engine: *eng.Engine) !Game {
+        return Game{ .actions = engine.createActionManager(GameActions, allocator) };
+    }
+    pub fn deinit(self: *Game) void {
+        self.actions.deinit();
+    }
+
+    pub fn update(self: *Game, engine: *Engine, dt: f32) void {
+        std.debug.print("[GAME] - updated called\n", .{});
+
+        if (engine.inputManager.keyboard.wasKeyJustPressed(.Esc)) {
+            std.debug.print("[RAW] Esc detected\n", .{});
+        }
+
+        if (self.actions.wasActionJustPressed(.Quit)) {
+            std.debug.print("I want to quit the engine\n", .{});
+            engine.stopRunning();
+        }
+
+        _ = dt;
+        return;
+    }
 };
-
-const GameActions = enum { Quit, Thrust, RotateLeft, RotateRight, Shoot };
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
+
+    const config = EngineConfig.toEngineNamedConfig("ZASTEROIDS");
+    var engine = try Engine.init(allocator, config);
+    defer engine.deinit();
+
+    var game = try Game.init(allocator, &engine);
+    try setupQuitBindings(&game.actions);
+
+    defer game.deinit();
+
+    try engine.run(&game);
+}
+
+fn setupQuitBindings(actions: *eng.input.ActionManager(GameActions)) !void {
+    try actions.addBinding(.{
+        .action = .Quit,
+        .source = .{ .Key = .Esc },
+    });
+
+    try actions.addBinding(.{
+        .action = .Quit,
+        .source = .{ .MouseButton = .Right },
+    });
+
+    try actions.addBinding(.{
+        .action = .Quit,
+        .source = .{ .KeyCombo = .{ .modifier = .Command, .key = .Q } },
+    });
 }
